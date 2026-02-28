6e94b25efbb1fb9573fc951c6f4c65a4345f5a43
iamsatyamjaiswal <iamsatyamjaiswal0@gmail.com>
2026-02-28 17:54:52 +0530

    Add Files

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'shop-manager-cache-v1';
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(['./', './index.html']);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
